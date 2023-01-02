d94c04a5f2a17c48168a9530029a134f5dbc54c1
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-02 20:17:05 +0200

    add cors to allow angular frontend from port 4200 to request data

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,24 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
-import schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Dependency

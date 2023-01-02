5eb665d1952e09600ea3c43a902f0b9a480bbd50
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-02 19:08:26 +0200

    board button component development

--- sampler/src/app/ui-components/board-button/board-button.component.ts
+++ sampler/src/app/ui-components/board-button/board-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-board-button',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-button.component.scss']
 })
 export class BoardButtonComponent implements OnInit {
+  @Input() public isAddBtn: boolean = false;
+  @Input() public title: string = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }

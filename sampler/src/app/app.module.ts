c2148b2d999dd1a8cfbc195a7cab212d8c4688eb
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-02 19:27:06 +0200

    rename first view to category-menu

--- sampler/src/app/app.module.ts
+++ sampler/src/app/app.module.ts
@@ -3,14 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { FrontMenuComponent } from './front-menu/front-menu.component';
-import { BoardButtonComponent } from './ui-components/board-button/board-button.component';
+import {BoardButtonComponent} from './ui-components/board-button/board-button.component';
+import {CategoryMenuComponent} from "./category-menu/category-menu.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontMenuComponent,
-    BoardButtonComponent
+    BoardButtonComponent,
+    CategoryMenuComponent,
   ],
   imports: [
     BrowserModule,

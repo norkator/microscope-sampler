535e1706f5b971a05b2abfa601d5bf30e76776c0
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-02 21:31:06 +0200

    emit modal close click back to parent

--- sampler/src/app/ui-components/modal/modal.component.ts
+++ sampler/src/app/ui-components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,13 +6,20 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
+  @Output() public modalClosedClick = new EventEmitter();
   @Input() public isOpen: boolean = false;
   @Input() public title: string = '';
+
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  public closeModal(): void {
+    this.isOpen = !this.isOpen
+    this.modalClosedClick.emit();
   }
 
 }

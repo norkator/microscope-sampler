544188217e5aa228e3a28592c2fd55335eab1d3d
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-03 13:54:59 +0200

    make category menu a default route

--- sampler/src/app/app-routing.module.ts
+++ sampler/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SampleGroupsComponent} from "./sample-groups/sample-groups.component";
 import {SampleComponent} from "./sample/sample.component";
+import {CategoryMenuComponent} from "./category-menu/category-menu.component";
 
 const routes: Routes = [
+  {path: '', component: CategoryMenuComponent},
   {path: 'sample-group', component: SampleGroupsComponent},
   {path: 'sample', component: SampleComponent},
 ];

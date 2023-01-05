bb01d0e97f15a039b10cae71ae7bf7372907ea99
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2023-01-05 17:39:52 +0200

    move camera features to its own ui component

--- sampler/src/app/samples/sample/sample.component.ts
+++ sampler/src/app/samples/sample/sample.component.ts
@@ -13,12 +13,6 @@ export class SampleComponent implements OnInit {
 
   public sampleFormGroup!: FormGroup;
 
-  // @ts-ignore
-  private constraints: any = window.constraints = {
-    audio: false,
-    video: true
-  };
-
   constructor(
     private formBuilder: FormBuilder,
   ) {
@@ -31,43 +25,6 @@ export class SampleComponent implements OnInit {
   }
 
   public saveSample(): void {
-  }
-
-  private handleSuccess(stream: any) {
-    const video = document.querySelector('video');
-    const videoTracks = stream.getVideoTracks();
-    console.log('Got stream with constraints:', this.constraints);
-    console.log(`Using video device: ${videoTracks[0].label}`);
-    // @ts-ignore
-    window.stream = stream;
-    // @ts-ignore
-    video.srcObject = stream;
-  }
-
-  handleError(error: any) {
-    if (error.name === 'OverconstrainedError') {
-      const v = this.constraints.video;
-      this.errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-    } else if (error.name === 'NotAllowedError') {
-      this.errorMsg('No permission allowed to access camera');
-    }
-    this.errorMsg(`getUserMedia error: ${error.name}`, error);
-  }
-
-  errorMsg(msg: any, error?: any) {
-    if (typeof error !== 'undefined') {
-      console.error(error);
-    }
-  }
-
-  public async init(e: any) {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
-      this.handleSuccess(stream);
-      e.target.disabled = true;
-    } catch (e) {
-      this.handleError(e);
-    }
   }
 
 }
--- sampler/src/app/ui-components/camera-image/camera-image.component.ts
+++ sampler/src/app/ui-components/camera-image/camera-image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-camera-image',
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CameraImageComponent implements OnInit {
 
-  constructor() { }
+  // @ts-ignore
+  private constraints: any = window.constraints = {
+    audio: false,
+    video: true
+  };
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  private handleSuccess(stream: any) {
+    const video = document.querySelector('video');
+    const videoTracks = stream.getVideoTracks();
+    console.log('Got stream with constraints:', this.constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    // @ts-ignore
+    window.stream = stream;
+    // @ts-ignore
+    video.srcObject = stream;
+  }
+
+  handleError(error: any) {
+    if (error.name === 'OverconstrainedError') {
+      const v = this.constraints.video;
+      this.errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+    } else if (error.name === 'NotAllowedError') {
+      this.errorMsg('No permission allowed to access camera');
+    }
+    this.errorMsg(`getUserMedia error: ${error.name}`, error);
+  }
+
+  errorMsg(msg: any, error?: any) {
+    if (typeof error !== 'undefined') {
+      console.error(error);
+    }
+  }
+
+  public async init(e: any) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+      this.handleSuccess(stream);
+      e.target.disabled = true;
+    } catch (e) {
+      this.handleError(e);
+    }
   }
 
 }
